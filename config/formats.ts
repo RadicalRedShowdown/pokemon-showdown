e66b3795f6ceb05e1fe17d8ff69f6e8f91066470
Karthik99999 <bandagondak0217@gmail.com>
2021-05-24 12:05:21 -0400

    clean up formats

--- config/formats.ts
+++ config/formats.ts
@@ -36,29 +36,29 @@ export const Formats: FormatList = [
 		name: "[Gen 8] RRDL",
 		mod: 'radred',
 		searchShow: false,
-		ruleset: ['Standard RadRed', 'Dynamax Clause', 'Z-Move Clause'],
+		ruleset: ['Standard', 'Dynamax Clause', 'Z-Move Clause'],
 		banlist: [
 			// mons
 			'Alakazam-Mega', 'Arceus', 'Blaziken-Mega', 'Calyrex-Ice', 'Calyrex-Shadow', 'Chandelure', 'Deoxys-Attack', 'Deoxys-Base', 'Deoxys-Speed', 'Dialga', 'Dragapult', 'Eternatus', 'Genesect', 'Gengar-Mega', 'Giratina', 'Greninja-Ash', 'Groudon', 'Ho-Oh', 'Kyogre', 'Kyurem-Black', 'Kyurem-White', 'Latias-Mega', 'Latios-Mega', 'Lucario-Mega', 'Lugia', 'Lunala', 'Magearna', 'Marshadow', 'Metagross-Mega', 'Mewtwo', 'Naganadel', 'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane', 'Necrozma-Ultra', 'Palkia', 'Pheromosa', 'Rayquaza', 'Reshiram', 'Salamence-Mega', 'Shaymin-Sky', 'Silvally', 'Solgaleo', 'Tapu Koko', 'Xerneas', 'Yveltal', 'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom', 'Zygarde-Complete',
 			// abilities
 			'Moody', 'Power Construct', 'Battle Bond',
 			// moves
-			'Swagger', 'Baton Pass', 'Dark Hole', 'Misty Explosion', 'Explosion', 'Self-Destruct',
+			'Swagger', 'Dark Hole', 'Misty Explosion', 'Explosion', 'Self-Destruct',
 			// complex bans
-			'Darkrai + Dark Hole', 'Blastoise-Mega + Shell Smash', 'Blaziken + Speed Boost', 'Kangaskhan-Mega + Seismic Toss', 'Greninja + Protean', 'Cinderace + Libero',
+			'Blastoise-Mega + Shell Smash', 'Blaziken + Speed Boost', 'Kangaskhan-Mega + Seismic Toss', 'Greninja + Protean', 'Cinderace + Libero',
 		],
 	},
 	{
 		name: "[Gen 8] OU",
 		mod: 'radred',
-		ruleset: ['Standard RadRed', 'Species Clause', 'Dynamax Clause', 'Z-Move Clause'],
-		banlist: ['Uber', 'AG', 'Moody', 'Power Construct', 'Shadow Tag', 'Arena Trap', 'Swagger', 'Baton Pass', 'Dark Hole', 'Misty Explosion'],
+		ruleset: ['Standard', 'Dynamax Clause', 'Z-Move Clause'],
+		banlist: ['Uber', 'AG', 'Moody', 'Power Construct', 'Shadow Tag', 'Arena Trap', 'Swagger', 'Misty Explosion'],
 	},
 	{
 		name: "[Gen 8] Ubers",
 		mod: 'radred',
-		ruleset: ['Standard RadRed', 'Species Clause', 'Dynamax Clause', 'Z-Move Clause', 'Mega Rayquaza Clause'],
-		banlist: ['AG', 'Baton Pass', 'Moody'],
+		ruleset: ['Standard', 'Dynamax Clause', 'Z-Move Clause', 'Mega Rayquaza Clause'],
+		banlist: ['AG', 'Moody'],
 	},
 	{
 		name: "[Gen 8] UU",
@@ -74,10 +74,11 @@ export const Formats: FormatList = [
 	{
 		name: "[Gen 8] Monotype",
 		mod: 'radred',
-		ruleset: ['Standard RadRed', 'Same Type Clause', 'Species Clause', 'Dynamax Clause', 'Z-Move Clause'],
+		ruleset: ['Standard', 'Same Type Clause', 'Dynamax Clause', 'Z-Move Clause'],
 		banlist: [
 			'AG', 'Uber',
-			'Battle Bond', 'Moody', 'Power Construct', 'Shadow Tag', 'Damp Rock', 'Smooth Rock', 'Terrain Extender', 'Baton Pass',
+			'Battle Bond', 'Moody', 'Power Construct', 'Shadow Tag',
+			'Damp Rock', 'Smooth Rock', 'Terrain Extender',
 			'Misty Explosion',
 		],
 	},
@@ -97,7 +98,7 @@ export const Formats: FormatList = [
 		name: "[Gen 8] RRC",
 		mod: 'radred',
 		gameType: 'doubles',
-		ruleset: ['Standard RadRed', 'Adjust Level = 50', 'Picked Team Size = 4', 'VGC Timer', 'Item Clause', 'Species Clause', 'Dynamax Clause', 'Z-Move Clause'],
+		ruleset: ['Standard', 'VGC Timer', 'Item Clause', 'Dynamax Clause', 'Z-Move Clause', 'Adjust Level = 50', 'Picked Team Size = 4'],
 		banlist: [
 			'Mewtwo',
 			'Unown', 'Ho-Oh', 'Lugia',
