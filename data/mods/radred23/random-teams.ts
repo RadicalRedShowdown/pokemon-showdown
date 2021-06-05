fefa64c7671f221c539c7b3a7d8ed73dfc7ab59a
Karthik99999 <bandagondak0217@gmail.com>
2021-06-05 15:40:14 -0400

    Randbats: add fix to double hp bug

--- data/mods/radred23/random-teams.ts
+++ data/mods/radred23/random-teams.ts
@@ -1210,6 +1210,10 @@ export class RandomRadicalRedTeams extends RandomTeams {
 			}
 			while (moves.size < 4 && rejectedPool.length) {
 				const moveid = this.sampleNoReplace(rejectedPool);
+				if (moveid.startsWith('hiddenpower')) {
+					if (hasHiddenPower) continue;
+					hasHiddenPower = true;
+				}
 				moves.add(moveid);
 			}
 
