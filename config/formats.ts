063863a551a18dcb7e97d1d5fe1c8e07c2aa6274
Clemen <pacman.ncr@gmail.com>
2023-02-03 10:54:35 +0700

    Revert "Remove RU from ladder"
    This reverts commit 23aa9da4b9104e06d94aa77e7b9222357e4d63c9.

--- config/formats.ts
+++ config/formats.ts
@@ -88,7 +88,6 @@ export const Formats: FormatList = [
 	{
 		name: "[Gen 8] RU",
 		mod: 'gen8rr',
-		searchShow: false,
 		ruleset: ['[Gen 8] UU'],
 		banlist: ['UU', 'RUBL', 'Trick Room', 'Ampharosite', 'Light Clay'],
 	},
