765586cddfb863a252df80f90354313321bf71fc
Clemen <pacman.ncr@gmail.com>
2026-05-19 08:25:20 +0100

    jumpscare line

--- data/mods/e4dlcap/abilities.ts
+++ data/mods/e4dlcap/abilities.ts
@@ -792,7 +792,8 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 				const details = pokemon.species.name + (pokemon.level === 100 ? '' : ', L' + pokemon.level) +
 					(pokemon.gender === '' ? '' : ', ' + pokemon.gender) + (pokemon.set.shiny ? ', shiny' : '');
 				this.add('replace', pokemon, details);
-				this.add('-end', pokemon, 'Jumpscare');
+				this.add('-end', pokemon, 'Jumpscare', '[silent]');
+				this.add('-message', `${pokemon.name} revealed its true nature!`);
 				this.add('raw|<center><img src="https://media.tenor.com/5QMrfOK5SZQAAAAM/fnaf-fnaf-memes.gif" height="96" alt="Jump Scare" /></center>');
 				if (this.ruleTable.has('illusionlevelmod')) {
 					this.hint("Illusion Level Mod is active, so this Pokemon's true level was hidden.", true);
